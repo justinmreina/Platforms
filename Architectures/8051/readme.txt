e58ffd6e02da7da28730f9e02a7adc6cbb3a0c65
Justin Reina <justinmreina@gmail.com>
2018-02-15 12:33:37 -0800

    (+)    initial commit

---
+++ Architectures/8051/readme.txt
@@ -0,0 +1,4 @@
+- this is simply Intel's 8051 architecture which Intel discontinued in 2007. In use now by others (TI, etc.)
+
+- 8-bit
+
---
+++ Intel/Curie/Dev Boards/Arduino 101/Arduino/Blink/curie101_blink/curie101_blink.ino
@@ -0,0 +1,15 @@
+
+#define GPIO_PIN_NUM  (0)
+
+
+void setup(void) {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(GPIO_PIN_NUM, OUTPUT);
+}
+
+void loop(void) {
+  digitalWrite(GPIO_PIN_NUM, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(GPIO_PIN_NUM, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
